852ee40d99420f38870ab7336b53d50327391975
IzzyKitty <115121463+IzzyKitty@users.noreply.github.com>
2023-03-19 18:26:22 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD9iNyPGSrgtVStxnHq1XqzSQU5XlYHS5s",
+    authDomain: "lets-chat-webapp-f158b.firebaseapp.com",
+    projectId: "lets-chat-webapp-f158b",
+    storageBucket: "lets-chat-webapp-f158b.appspot.com",
+    messagingSenderId: "963307460292",
+    appId: "1:963307460292:web:94da5e36aab047eb595b91"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
